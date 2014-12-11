6d13d0d82fc4e92f07115d3d051fa2fc941dd5d0
jayro <jayrore@hotmail.com>
2014-12-11 15:01:51 -0600

    start

---
+++ Fonadin/Mongo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace Fonadin
+{
+    class Mongo
+    {
+        private MongoUrl urlCon = new MongoUrl("mongodb://jayroserver-pc");
+        private MongoClient client;
+        private MongoServer server;
+        private MongoDatabase db;
+        public MongoCollection collBook;
+        public Mongo()
+        {
+            client = new MongoClient(urlCon);
+            server = client.GetServer();
+            db = server.GetDatabase("example");
+            collBook = db.GetCollection("Book");
+        }
+     
+    }
+}
